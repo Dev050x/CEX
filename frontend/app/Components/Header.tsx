08e1e126ef0e8ce3a70f471ab1aa4834f5894af4
0xDev05 <divpatel1274@gmail.com>
2026-01-26 20:47:51 +0530

    feature(UI): implement base structure of UI

--- frontend/app/Components/Header.tsx
+++ frontend/app/Components/Header.tsx
@@ -6,20 +6,20 @@ const Header = () => {
                     <div className="flex items-center flex-row">
                         <a
                             href="/"
-                            className="flex items-center justify-center h-8 text-sm font-semibold mr-3 ml-4 hover:opacity-90 shrink-0"
+                            className="flex items-center justify-center h-8 text-sm font-semibold ml-4 hover:opacity-90 shrink-0"
                         >
                             Exchange
                         </a>
 
                         <a
                             href="/"
-                            className="flex items-center justify-center h-8 text-sm font-semibold mr-3 ml-4 hover:opacity-90 shrink-0"
+                            className="flex items-center justify-center h-8 text-sm font-semibold mx-4 hover:opacity-90 shrink-0"
                         >
                             Trade
                         </a>
 
                     </div>
-                    <div className="flex justify-end gap-4">
+                    <div className="flex justify-end gap-4 mx-4">
                         <a className="rounded-lg bg-[#202127] px-3 py-1.5 text-sm font-semibold text-white hover:opacity-90" href="/login">
                             Deposit
                         </a>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,31 @@
 import Header from "./Components/Header";
-import { getKlines, getTickers, getTrades } from "./utils/httpClient";
 
 export default async function Home() {
-  const data = await getTickers();
-  console.log(data);
+
   return (
-    <div className="flex flex-col max-h-screen min-h-screen overflow-y-hidden">
-      <div className="bg-[#0e0f14] sticky top-0 z-20 w-full">
+    <div className="bg-[#0B0E11] flex flex-col max-h-screen min-h-screen overflow-y-hidden gap-2">
+      <div className="bg-[#181a20] sticky top-0 z-20 w-full">
         <Header />
       </div>
-      <div>Hero</div>
+      <div className="bg-[#181A20] text-high-emphasis flex flex-1 flex-col justify-between overflow-auto mx-25">
+            <div className="flex flex-row h-full w-full flex-1 gap-2 px-4">
+              <div className="flex flex-col flex-1 border gap-1">
+                <div className="border h-16">Market Bar</div>
+                <div className="flex flex-row flex-1 border">
+                  <div className="border w-[380px]">orderbook</div>
+                  <div className="border flex flex-col flex-1">
+                    <div className="border h-[524px]">Trading View</div>
+                    <div className="border flex-1">Swap UI</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col w-[380px] border">
+                <div className="border h-[419px]">Coins</div>
+                <div className="border flex-1">Recent Trades</div>
+              </div>
+            </div>
+      </div>
     </div>
   );
 }
