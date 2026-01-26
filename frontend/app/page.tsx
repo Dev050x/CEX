0ef05bfdada88200647faf733d0aed4225eaa23d
0xDev05 <divpatel1274@gmail.com>
2026-01-26 19:34:59 +0530

    feat(Frontend-Header): implement Header

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,15 @@
+import Header from "./Components/Header";
 import { getKlines, getTickers, getTrades } from "./utils/httpClient";
 
 export default async function Home() {
   const data = await getTickers();
   console.log(data);
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     
+    <div className="flex flex-col max-h-screen min-h-screen overflow-y-hidden">
+      <div className="bg-[#0e0f14] sticky top-0 z-20 w-full">
+        <Header />
+      </div>
+      <div>Hero</div>
     </div>
   );
 }
