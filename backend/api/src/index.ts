9ef165b3461619301ecf9012594b6f8e5b59bb98
0xDev05 <divpatel1274@gmail.com>
2026-02-10 16:38:13 +0530

    TODO(ticker): ticker endpoint

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { orderRouter } from "./routes/order.js";
 import { depthRouter } from "./routes/depth.js";
 import { klineRouter } from "./routes/kline.js";
+import { tickersRouter } from "./routes/ticker.js";
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use("/api/v1/order",orderRouter);
 app.use("/api/v1/depth", depthRouter);
 app.use("/api/v1/klines", klineRouter);
+app.use("/api/v1/tickers", tickersRouter);
 
 app.listen(3000, () => {
     console.log(`hello from the server ${3000}`);
