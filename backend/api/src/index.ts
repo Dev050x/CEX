541491070468d6427ab7418d3ffcfdc63cfd9ac0
0xDev05 <divpatel1274@gmail.com>
2026-02-10 16:35:58 +0530

    feat(kline): implement kline endpoint

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { orderRouter } from "./routes/order.js";
 import { depthRouter } from "./routes/depth.js";
+import { klineRouter } from "./routes/kline.js";
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use("/api/v1/order",orderRouter);
 app.use("/api/v1/depth", depthRouter);
+app.use("/api/v1/klines", klineRouter);
 
 app.listen(3000, () => {
     console.log(`hello from the server ${3000}`);
