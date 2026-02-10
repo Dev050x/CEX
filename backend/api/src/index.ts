634c0e5e0bd32107f66c75709711307ea99b0868
0xDev05 <divpatel1274@gmail.com>
2026-02-10 16:19:10 +0530

    feat(depth): implement depth endpoint

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { orderRouter } from "./routes/order.js";
+import { depthRouter } from "./routes/depth.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/order",orderRouter);
+app.use("/api/v1/depth", depthRouter);
 
 app.listen(3000, () => {
     console.log(`hello from the server ${3000}`);
